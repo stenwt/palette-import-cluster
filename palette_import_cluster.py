cc9ad4773c5a881a904aac872a82c639536aeb52
Sten Turpin <sten@spectrocloud.com>
2022-12-22 17:44:49 -0600

    conform (mostly) to pylint, fix failure in first pass creating a new cluster to import

--- palette_import_cluster.py
+++ palette_import_cluster.py
@@ -10,11 +10,11 @@ __version__ = "0.1.0"
 __license__ = "GPL 3.0"
 
 import argparse
-import requests
 import json
 import sys
+import requests
 from logzero import logger
-
+from requests.adapters import HTTPAdapter, Retry
 
 def main(args):
     """ Main entry point of the app """
@@ -27,49 +27,55 @@ def get_clusters(api_endpoint, session):
     """get list of all clusters from the API"""
     logger.info("getting list of existing clusters")
     try:
-      clusters = session.get(api_endpoint + "/v1/spectroclusters")
-      clusters.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-      logger.error(e.response.text)
-      sys.exit(1)
+        clusters = session.get(api_endpoint + "/v1/spectroclusters")
+        clusters.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        logger.error(error.response.text)
+        sys.exit(1)
     return clusters
 
 def import_cluster(api_endpoint, cloud_type, cluster_name, session):
     """import a new cluster with its name"""
     imported_cluster = {
-      "metadata": {
-        "name": cluster_name
-      },
-      "spec": {
-        "clusterConfig": {
+        "metadata": {
+            "name": cluster_name
+        },
+        "spec": {
+            "clusterConfig": {
+            }
         }
-      }
     }
-    logger.info("importing a new cluster named" + cluster_name)
-    try: 
-      imported_uid = session.post(api_endpoint + "/v1/spectroclusters/" + cloud_type + "/import", json=imported_cluster)
-      imported_uid.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-      logger.error(e.response.text)
-      sys.exit(1)
+    logger.info("importing a new cluster named " + cluster_name)
+    try:
+        imported_cluster = session.post(api_endpoint + "/v1/spectroclusters/"
+            + cloud_type + "/import", json=imported_cluster)
+        imported_cluster.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        logger.error(error.response.text)
+        sys.exit(1)
+    imported_uid = imported_cluster.text.split("\"")[3]
+    logger.info("imported a new cluster with uid " + imported_uid)
     return imported_uid
 
 def get_manifest(api_endpoint, cluster_uid, session):
     """print the imported cluster manifest"""
-    logger.info("get the import manifest for cluster with id" + cluster_uid)
+    logger.info("get the import manifest for cluster with id " + cluster_uid)
     try:
-      imported_manifest = session.get(api_endpoint + "/v1/spectroclusters/" + cluster_uid + "/import/manifest")
-      imported_manifest.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-      logger.error(e.response.text)
-      sys.exit(1)
-    return(imported_manifest.text)
+        imported_manifest = session.get(api_endpoint + "/v1/spectroclusters/"
+            + cluster_uid + "/import/manifest")
+        imported_manifest.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        logger.error(error.response.text)
+        sys.exit(1)
+    return imported_manifest.text
 
 def setup_session(args):
     """set the required parameters to auth to the API"""
     session = requests.Session()
     session.headers.update({'ApiKey': args.api_key})
     session.headers.update({'ProjectUid': args.project_uid})
+    retries = Retry(total=5, backoff_factor=1, status_forcelist=[404])
+    session.mount('https://', HTTPAdapter(max_retries=retries))
     return session
 
 def get_cluster_uid(args, session):
@@ -77,46 +83,49 @@ def get_cluster_uid(args, session):
     clusters = json.loads(get_clusters(args.api_endpoint, session).text)
     cluster_uid = None
     for cluster in clusters["items"]:
-      if cluster["metadata"]["name"] == args.cluster_name[0]:
-        logger.info("found existing cluster " + cluster["metadata"]["name"])
-        # ignore deleted clusters
-        if "deleted" in cluster["metadata"]["annotations"]:
-          logger.info("ignoring deleted cluster " + cluster["metadata"]["name"])
-          pass
-        # stop if we find a cluster with our name
-        else:
-          logger.info("will get manifest for existing cluster " + cluster["metadata"]["name"] +
-            " with id " + cluster["metadata"]["uid"])
-          cluster_uid = cluster["metadata"]["uid"]
-          break
-    if cluster_uid == None:
-      logger.info("no cluster found, doing import")
-      cluster_uid = import_cluster(args.api_endpoint, args.cloud_type, args.cluster_name[0], session)
+        if cluster["metadata"]["name"] == args.cluster_name[0]:
+            logger.info("found existing cluster " + cluster["metadata"]["name"]
+                + " with uid " + cluster["metadata"]["uid"])
+            # ignore deleted clusters
+            if "deleted" in cluster["metadata"]["annotations"]:
+                logger.info("ignoring deleted cluster " + cluster["metadata"]["name"]
+                    + " with uid " + cluster["metadata"]["uid"])
+            # stop if we find a cluster with our name
+            else:
+                logger.info("will get manifest for existing cluster " +
+                    cluster["metadata"]["name"] + " with id " + cluster["metadata"]["uid"])
+                cluster_uid = cluster["metadata"]["uid"]
+                break
+    if cluster_uid is None:
+        logger.info("no cluster found, doing import")
+        cluster_uid = import_cluster(args.api_endpoint, args.cloud_type,
+            args.cluster_name[0], session)
     return cluster_uid
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
     parser = argparse.ArgumentParser()
 
     # Name of the cluster to import
     parser.add_argument("cluster_name", help="name of the cluster to import", nargs=1)
 
     # Allow configuring API endpoint
-    parser.add_argument("-a", "--api-endpoint", dest="api_endpoint", action="store", 
-      default="https://api.spectrocloud.com", help="optionally configure the API endpoint location")
+    parser.add_argument("-a", "--api-endpoint", dest="api_endpoint", action="store",
+        default="https://api.spectrocloud.com",
+        help="optionally configure the API endpoint location")
 
     # Project UID is a required parameter
-    parser.add_argument("-u", "--project-uid", dest="project_uid", action="store", required=True,
-      help="project UID - find in your Palette profile screen")
+    parser.add_argument("-u", "--project-uid", dest="project_uid", action="store",
+        required=True, help="project UID - find in your Palette profile screen")
 
     # API key is a required parameter
     parser.add_argument("-k", "--api-key", dest="api_key", action="store", required=True,
-      help="API key - find in your Palette profile screen")
+        help="API key - find in your Palette profile screen")
 
     # Cloud type is a required parameter - cloud types map to API endpoints
-    cloud_types=['aws','azure','edge-native','edge','gcp','generic','libvirt','maas','openstack','vsphere']
-    parser.add_argument("-t", "--cloud-type", choices=cloud_types, action="store", required=True,
-      help="set the cloud type to one of the supported options")
+    cloud_types=['aws','azure','edge-native','edge','gcp','generic','libvirt'
+        ,'maas','openstack','vsphere']
+    parser.add_argument("-t", "--cloud-type", choices=cloud_types, action="store",
+        required=True, help="set the cloud type to one of the supported options")
 
     # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
     parser.add_argument(
@@ -134,4 +143,3 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     main(args)
-
